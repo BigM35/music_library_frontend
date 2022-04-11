df2bdb6f1a85d58a5b23b4b07a9f4d7c3ce6726a
Manny M <bigm@Mannys-MacBook-Pro.local>
2022-04-11 16:19:53 -0500

    installed Axios to the packege to the package.json + implemented useState and useEffect in the App.jsx to handle the get all part of the cru d

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
+import axios from 'axios';
+import DisplayMusic from './components/DisplayMusic/DisplayMusic'
 
+const BASEURL = 'http://127.0.0.1:8000/music/'
 
 function App() {
-  const [songInfo, setSongInfo] = useState({title: '', artist: '', album: '', release_date: '', genre: ''});
-  
+
+  const [songInfo, setSongInfo] = useState([]);
+
+  useEffect(() => {
+    getMusic();
+  }, []);
+
+  async function getMusic() {
+    try {
+      let response = await axios.get(BASEURL)
+      console.log("GetMusic Response: ", response.data)
+      setSongInfo(response.data)
+    } catch (err) {
+      console.log(`Error: ${err}`);
+    }
+  }
+
+
+
   return (
-    <div>
-      hello world
-    </div>
+    <Fragment>
+      <DisplayMusic.jsx music = {songInfo}/>
+    {/* <DisplayMusic /> */}
+    </Fragment>
   );
 }
 
